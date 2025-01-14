c2b4a8af926d140708559d2d9a61ea21f54256fb
SaraToth <saratoth101@gmail.com>
2025-01-14 17:58:43 +0900

    Write function isValidVertex that checks if the start and end vertex are in the playable gameboard and throws error if not

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function knightMoves(start, end) {
+    if(!isValidVertex(start)) {
+        throw new Error("Start vertex exceeds the game board.");
+    }
+
+    if(!isValidVertex(end)) {
+        throw new Error("End vertex exceeds the game board.");
+    }
 
     return {
-        // isValidVertex: function(vertex) {} - to check if vertex is in board
         
         possibleMoves: function(vertex) {
             const x = vertex[0];
@@ -38,5 +44,16 @@ function knightMoves(start, end) {
     }
 }
 
-const graph = knightMoves([3, 3]);
+function isValidVertex(vertex) {
+    const x = vertex[0];
+    const y = vertex[1];
+
+    if(x < 0 || x > 7 || y < 0 || y > 7) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const graph = knightMoves([3, 3], [4, 3]);
 console.log(graph.possibleMoves([3, 3]));
